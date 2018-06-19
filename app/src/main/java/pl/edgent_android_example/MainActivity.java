bc30f44e8425f60c4de6721245df56d5ac18f851
wgrabis <wgrabis@gmail.com>
2018-06-20 00:16:10 +0200

    Fix of library add in progress

--- app/src/main/java/pl/edgent_android_example/MainActivity.java
+++ app/src/main/java/pl/edgent_android_example/MainActivity.java
@@ -21,6 +21,17 @@ import org.slf4j.Logger;
 
 import java.nio.ByteBuffer;
 
+import pl.edu.agh.edgentandroidwrapper.Topology.FilteringTopology;
+import pl.edu.agh.edgentandroidwrapper.collector.SensorDataCollector;
+import pl.edu.agh.edgentandroidwrapper.filter.ValueInRangeFilter;
+import pl.edu.agh.edgentandroidwrapper.helper.MqttVisitor;
+import pl.edu.agh.edgentandroidwrapper.helper.StreamVisitor;
+import pl.edu.agh.edgentandroidwrapper.samplingrate.SamplingRate;
+import pl.edu.agh.edgentandroidwrapper.task.EdgentTask;
+
+import static android.hardware.Sensor.TYPE_AMBIENT_TEMPERATURE;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class MainActivity extends AppCompatActivity {
     private final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(this.getClass().getName());
 
@@ -30,40 +41,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create Edgent topology
-        final DirectProvider provider = new DirectProvider();
-        final Topology topology = provider.newTopology("My topology");
-
-        // create Edgent stream using Android sensors
         final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        final TStream<SensorEvent> stream = SensorStreams.sensors(topology, sensorManager, Sensor.TYPE_ACCELEROMETER);
+
+        StreamVisitor<SensorEvent> visitor = MqttVisitor.<SensorEvent>builder()
+                .queueUrl("tcp://iot.eclipse.org:1883")
+                .clientId("52035_client")
+                .topic("/52035_topic")
+                .serializer(new Function<SensorEvent, byte[]>() {
+                    @Override
+                    public byte[] apply(SensorEvent sensorEvent) {
+                        return ByteBuffer.allocate(4).putFloat(sensorEvent.values[0]).putFloat(4, sensorEvent.values[1]).array();
+                    }
+                })
+                .build();
+
+        EdgentTask task = EdgentTask.builder()
+                .sensorManager(sensorManager)
+                .sensorDataCollector(
+                        SensorDataCollector.builder()
+                                .sensor(Sensor.TYPE_ACCELEROMETER)
+                                .samplingRate(
+                                        SamplingRate.builder()
+                                                .timeUnit(SECONDS)
+                                                .value(2)
+                                                .build()
+                                )
+                                .simpleTopology(
+                                        FilteringTopology.builder()
+                                                .name("This is topology name")
+                                                .tag("TEMPERATURE")
+                                                .predefinedFilter(
+                                                        ValueInRangeFilter.builder()
+                                                                .rangeStart(60.0)
+                                                                .rangeEnd(90.0)
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .streamVisitor(visitor)
+                                .build())
+                .build();
+
+        task.start();
+
+
+        // create Edgent topology
+//        final DirectProvider provider = new DirectProvider();
+//        final Topology topology = provider.newTopology("My topology");
+//
+//        // create Edgent stream using Android sensors
+//        final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        final TStream<SensorEvent> stream = SensorStreams.sensors(topology, sensorManager, Sensor.TYPE_ACCELEROMETER);
 
         // register Android ui thread consumer
-        final TextView textView = findViewById(R.id.example_textview);
-        final Consumer<SensorEvent> uiThreadConsumer = new Consumer<SensorEvent>() {
-            @Override
-            public void accept(SensorEvent event) {
-                textView.setText(String.valueOf(event.values[0] + " , " + event.values[1]));
-            }
-        };
-        ActivityStreams.sinkOnUIThread(this, stream, uiThreadConsumer);
-
-        // register MQTT consumer
-        final Function<SensorEvent, byte[]> mqttSerializer = new Function<SensorEvent, byte[]>() {
-            @Override
-            public byte[] apply(SensorEvent event) {
-                return ByteBuffer.allocate(8).putFloat(event.values[0]).putFloat(4, event.values[1]).array();
-            }
-        };
-        MqttStreams mqttStreams;
-        mqttStreams = new MqttStreams(topology, "tcp://iot.eclipse.org:1883", "52033_client");
-        mqttStreams.publish(stream, of("/52033_topic"), mqttSerializer, of(0), of(false));
+//        final TextView textView = findViewById(R.id.example_textview);
+//        final Consumer<SensorEvent> uiThreadConsumer = new Consumer<SensorEvent>() {
+//            @Override
+//            public void accept(SensorEvent event) {
+//                textView.setText(String.valueOf(event.values[0] + " , " + event.values[1]));
+//            }
+//        };
+//        ActivityStreams.sinkOnUIThread(this, stream, uiThreadConsumer);
+//
+//        // register MQTT consumer
+//        final Function<SensorEvent, byte[]> mqttSerializer = new Function<SensorEvent, byte[]>() {
+//            @Override
+//            public byte[] apply(SensorEvent event) {
+//                return ByteBuffer.allocate(8).putFloat(event.values[0]).putFloat(4, event.values[1]).array();
+//            }
+//        };
+//        MqttStreams mqttStreams;
+//        mqttStreams = new MqttStreams(topology, "tcp://iot.eclipse.org:1883", "52033_client");
+//        mqttStreams.publish(stream, of("/52033_topic"), mqttSerializer, of(0), of(false));
 
 
 //        TStream<String> s = top.constants("one", "two", "three");
 //        mqttStreams.publish(s, "myTopic", 0);
         // run Edgent topology with both consumers
-        provider.submit(topology);
+//        provider.submit(topology);
 
     }
 
